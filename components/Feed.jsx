a20288ea4bffa5afab0f4e600eec10f6e792d77c
3xp1o1t <jes.monti.503@gmail.com>
2023-07-12 12:01:43 -0600

    Added search capabilities to feed component
    Added search for tag, prompt text and username
    Added click on tag for search

--- components/Feed.jsx
+++ components/Feed.jsx
@@ -1,4 +1,4 @@
-'use client';
+"use client";
 
 import { useState, useEffect } from "react";
 import PromptCard from "@components/PromptCard";
@@ -14,34 +14,64 @@ const PromptCardList = ({ data, handleTagClick }) => {
         />
       ))}
     </div>
-  )
-}
+  );
+};
 
 const Feed = () => {
+  // Search states
+  const [searchText, setSearchText] = useState("");
+  const [searchTimeout, setSearchTimeout] = useState(null);
+  const [searchedResults, setSearchedResults] = useState([]);
 
-  const [searchText, setSearchText] = useState('');
   const [posts, setPosts] = useState([]);
 
-  const handleSearchChange = (e) => {
 
-  }
+  const fetchPosts = async () => {
+    const response = await fetch("/api/prompt");
+    const data = await response.json();
+
+    setPosts(data);
+  };
 
   useEffect(() => {
-    const fetchPosts = async () => {
-      const response = await fetch('/api/prompt');
-      const data = await response.json();
-
-      setPosts(data);
-    }
-    
     fetchPosts();
-  }, [])
-  
+  }, []);
+
+  // search logic
+  const filterPrompts = (searchText) => {
+    const regex = new RegExp(searchText, "i"); // i flag for case-insensitive search
+    return posts.filter(
+      (item) => 
+        regex.test(item.creator.username) ||
+        regex.test(item.tag) ||
+        regex.test(item.prompt)
+    );
+  };
+
+  const handleSearchChange = (e) => {
+    clearTimeout(searchTimeout);
+    setSearchText(e.target.value);
+
+    // debounce method
+    setSearchTimeout(
+      setTimeout(() => {
+        const searchResult = filterPrompts(e.target.value);
+        setSearchedResults(searchResult);
+      }, 500)
+    );
+  };
+
+  const handleTagClick = (tagName) => {
+    setSearchText(tagName);
+    const searchResult = filterPrompts(tagName);
+    setSearchedResults(searchResult);
+  }
 
   return (
+
     <section className="feed">
       <form className="relative w-full flex-center">
-        <input 
+        <input
           type="text"
           placeholder="Search for a tag or a username"
           value={searchText}
@@ -49,15 +79,17 @@ const Feed = () => {
           required
           autoFocus
           className="search_input peer"
-          />
+        />
       </form>
 
-      <PromptCardList
-        data = {posts}
-        handleTagClick={() => {}}
-      />
+      {searchText ? (
+      <PromptCardList data={searchedResults} handleTagClick={handleTagClick} />
+      ) : (
+        <PromptCardList data={posts} handleTagClick={handleTagClick} />
+      )}
+
     </section>
-  )
-}
+  );
+};
 
 export default Feed;
